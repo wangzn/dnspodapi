d2db0a8b7493570c48e57dbfefe184ce64f7ff75
wangzn <wangzn04@gmail.com>
2018-08-31 21:43:13 +0800

    finish basic info command

--- info.go
+++ info.go
@@ -7,56 +7,23 @@ package dnspodapi
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 	"reflect"
 	// "github.com/wangzn/dnspodapi"
 )
 
-// ModuleName defines the module name used in API
-const ModuleName = "Info"
+// InfoModuleName defines the module name used in API
+const InfoModuleName = "info"
 
 var info Info
 
 // Info defines the struct
-type Info struct{}
-
-func init() {
-	Register(ModuleName, infoReflectFunc)
-}
-
-// infoReflectFunc defines the reflect func
-func infoReflectFunc(action string, data Params) ActionResult {
-	code, bs, err := HTTPResp(ModuleName, action, nil, nil)
-	if err != nil {
-		return ActionResult{
-			Code: code,
-			Err:  err,
-		}
-	}
-	in := []reflect.Value{reflect.ValueOf(bs)}
-	out := reflect.ValueOf(info).Call(in)
-	if len(out) == 0 {
-		return ActionResult{
-			Code: 1001,
-			Err:  fmt.Errorf("invalid call func result: not enough result"),
-		}
-	}
-	return ActionResult{
-		Code: 1000,
-		Err:  nil,
-		Data: out[0].Interface(),
-	}
+type Info struct {
+	A string
 }
 
 // Version returns version info
-func (i Info) version(bs []byte) interface{} {
-	//{
-	//	"status": {
-	//		"code": "1",
-	//		"message": "4.6",
-	//		"created_at": "2012-09-10 11:20:39"
-	//	}
-	//}
+func (i Info) Version(bs []byte) interface{} {
 	var d struct {
 		Status RespCommon `json:"status"`
 	}
@@ -65,4 +32,16 @@ func (i Info) version(bs []byte) interface{} {
 		return err
 	}
 	return d
+}
+
+func init() {
+	info = Info{}
+	Register(InfoModuleName, infoReflectFunc)
+}
+
+// infoReflectFunc defines the reflect func
+func infoReflectFunc(action string, data Params) ActionResult {
+	log.Println("in info reflect func, aciton:", action)
+	return callReflectFunc(reflect.ValueOf(info), InfoModuleName, action, nil,
+		nil)
 }
