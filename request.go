42438dcec0a08bde8f342ad023e99285c8a06ed3
wangzn <wangzn04@gmail.com>
2018-08-31 16:40:24 +0800

    update code for info basic impl

--- request.go
+++ request.go
@@ -4,3 +4,12 @@
 // @Date: 2018-08-31 14:55:56
 
 package dnspodapi
+
+import "time"
+
+// RespCommon defines common struct in http endpoint
+type RespCommon struct {
+	Code      string    `json:"code"`
+	Message   string    `json:"message"`
+	CreatedAt time.Time `json:"created_at"`
+}
